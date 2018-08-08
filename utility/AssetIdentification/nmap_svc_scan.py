945e797470557014cd2f44557af47ec7d7a449c0
lotuc <lotu.c@outlook.com>
2018-08-09 03:09:29 +0800

    utility: 更新 nmap 扫描脚本对 URL 的处理

--- utility/AssetIdentification/nmap_svc_scan.py
+++ utility/AssetIdentification/nmap_svc_scan.py
@@ -7,49 +7,55 @@ import logging
 import os
 import re
 import tempfile
+from urlparse import urlparse
 
 from common import COMMON_PORTS
 from common import create_cmd_parser as create_common_cmd_parser
 from common import print_result, setup_logger
 
-NMAP_CMD = '{nmap_bin} -p {ports} -oN {output_file} {hosts}'
+NMAP_CMD = "{nmap_bin} -p {ports} -oN {output_file} {hosts}"
 
 
-def run_nmap(hosts, outfile, nmap_bin, ports=COMMON_PORTS):
-    '''执行 nmap 进行服务扫描'''
+def run_nmap(host_or_url, outfile, nmap_bin, ports=COMMON_PORTS):
+    """执行 nmap 进行服务扫描"""
+    parsed = urlparse(host_or_url)
+    host = host_or_url
+    if parsed.scheme:
+        host = parsed.netloc
     if isinstance(ports, (set, list, tuple)):
-        ports = ','.join([str(x) for x in ports])
+        ports = ",".join([str(x) for x in ports])
     cmd = NMAP_CMD.format(
-        nmap_bin=nmap_bin, ports=ports, output_file=outfile, hosts=hosts)
-    logging.info('执行: %s', cmd)
+        nmap_bin=nmap_bin, ports=ports, output_file=outfile, hosts=host
+    )
+    logging.info("执行: %s", cmd)
     os.system(cmd)
 
 
 def create_cmd_parser():
     parser = create_common_cmd_parser()
     parser.add_argument(
-        '--nmap-bin', required=False, dest='nmap_bin',
-        help='NMAP 二进制文件')
+        "--nmap-bin", required=False, dest="nmap_bin", help="NMAP 二进制文件"
+    )
     return parser
 
 
 def parse_nmap_result(nmap_outfile):
     result = {}
-    portpattern = re.compile(r'(\d+)/')
-    namepattern = re.compile(r'open\s*(.*)\d*')
+    portpattern = re.compile(r"(\d+)/")
+    namepattern = re.compile(r"open\s*(.*)\d*")
     with open(nmap_outfile) as outf:
         for line in outf:
-            if 'open' not in line:
+            if "open" not in line:
                 continue
             port = portpattern.findall(line)
             name = namepattern.findall(line)
             port = port[0] if port is not None else None
             name = name[0] if name is not None else None
-            result[name] = {'port': port}
+            result[name] = {"port": port}
     return result
 
 
-def main(nmap_bin='/usr/bin/nmap'):
+def main(nmap_bin="/usr/bin/nmap"):
     parser = create_cmd_parser()
     args = parser.parse_args()
     nmap_bin = args.nmap_bin or nmap_bin
@@ -60,5 +66,5 @@ def main(nmap_bin='/usr/bin/nmap'):
     print_result(result, args.json_out_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- utility/AssetIdentification/what_web.py
+++ utility/AssetIdentification/what_web.py
@@ -9,18 +9,17 @@ from urlparse import urlparse
 
 from common import create_cmd_parser, print_result, setup_logger
 
-WHAT_WEB_CMD = '{what_web_bin} {url} --log-json {output_file}'
+WHAT_WEB_CMD = "{what_web_bin} {url} --log-json {output_file}"
 
 
 def run_whatweb(url, outfile, what_web_bin):
-    cmd = WHAT_WEB_CMD.format(
-        what_web_bin=what_web_bin, url=url, output_file=outfile)
-    logging.info('执行: %s', cmd)
+    cmd = WHAT_WEB_CMD.format(what_web_bin=what_web_bin, url=url, output_file=outfile)
+    logging.info("执行: %s", cmd)
     os.system(cmd)
 
 
 class WhatWebResultParser(object):
-    '''whatweb 结果解析'''
+    """whatweb 结果解析"""
 
     def __init__(self, whatweb_outfile):
         with open(whatweb_outfile) as outfile:
@@ -33,66 +32,69 @@ class WhatWebResultParser(object):
         return self.components
 
     def _parse_one(self, json_obj):
-        '''解析某个结果'''
-        logging.debug('解析：%s', json_obj)
+        """解析某个结果"""
+        logging.debug("解析：%s", json_obj)
         if json_obj == {}:
             return
-        target = json_obj['target']
+        target = json_obj["target"]
         pth = urlparse(target).path
-        plugins = json_obj['plugins']
+        plugins = json_obj["plugins"]
         for name in plugins:
-            if name in ('HTTPServer', 'X-Powered-By'):
+            if name in ("HTTPServer", "X-Powered-By"):
                 self._parse_common(plugins[name])
-            elif name == 'MetaGenerator':
+            elif name == "MetaGenerator":
                 self._parse_meta_generator(plugins[name], pth)
 
     def _parse_common(self, json_obj):
-        '''结果为 组件名/版本 的形式的结果的解析'''
-        for item in json_obj.get('string', []):
-            item = item.split(' ')[0]
+        """结果为 组件名/版本 的形式的结果的解析"""
+        for item in json_obj.get("string", []):
+            item = item.split(" ")[0]
             (name, version) = (item, None)
-            if '/' in item:
-                name, version = item.split('/', 1)
-            elif '(' in item:
-                name, _version = item.split('(')
-                version = _version.split(')')[0]
+            if "/" in item:
+                name, version = item.split("/", 1)
+            elif "(" in item:
+                name, _version = item.split("(")
+                version = _version.split(")")[0]
             name = self._common_name_calibration(name)
             info = self.components.get(name, {})
             if version:
-                info['version'] = version
+                info["version"] = version
             self.components[name] = info
-    
+
     def _common_name_calibration(self, name, component_path=None):
-        '''转译组件名'''
+        """转译组件名"""
         # 默认组件位置
         if not component_path:
-            CScan_POC_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-            component_path = os.path.join(CScan_POC_dir, "CScanPoc/CScanPoc/resources/component")
+            CScan_POC_dir = os.path.dirname(
+                os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            )
+            component_path = os.path.join(
+                CScan_POC_dir, "CScanPoc/CScanPoc/resources/component"
+            )
         all_component = os.listdir(component_path)
         for component in all_component:
             if component.endswith(".json"):
-                component_name = component.split('.json')[0]
+                component_name = component.split(".json")[0]
                 if name.upper() in component_name.upper():
                     return component_name
                 else:
                     return name
 
-
     def _parse_meta_generator(self, json_obj, pth=None):
-        '''MetaGenerator 结果的解析'''
-        for item in json_obj.get('string', []):
+        """MetaGenerator 结果的解析"""
+        for item in json_obj.get("string", []):
             (name, version) = (item, None)
-            if ' ' in item:
-                (name, version) = item.split(' ', 1)
-            name = item.split(' ')[0]
+            if " " in item:
+                (name, version) = item.split(" ", 1)
+            name = item.split(" ")[0]
             info = self.components.get(name, {})
             if version:
-                info['version'] = version
-            info['deploy_path'] = pth or '/'
+                info["version"] = version
+            info["deploy_path"] = pth or "/"
             self.components[name] = info
 
 
-def main(what_web_bin='WhatWeb/whatweb'):
+def main(what_web_bin="WhatWeb/whatweb"):
     parser = create_cmd_parser()
     args = parser.parse_args()
     setup_logger()
@@ -102,5 +104,5 @@ def main(what_web_bin='WhatWeb/whatweb'):
     print_result(result, args.json_out_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
