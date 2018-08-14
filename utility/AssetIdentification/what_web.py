117f19f7ef99719a7f9e630a0c1e169c9ede40e0
lotuc <lotu.c@outlook.com>
2018-08-15 00:49:00 +0800

    utility: 更新组件识别，修复名字翻译

--- utility/AssetIdentification/what_web.py
+++ utility/AssetIdentification/what_web.py
@@ -12,8 +12,16 @@ from common import create_cmd_parser, print_result, setup_logger
 WHAT_WEB_CMD = "{what_web_bin} {url} --log-json {output_file}"
 
 
+def _safe_lower(s):
+    try:
+        return s.lower()
+    except:
+        return s
+
+
 def run_whatweb(url, outfile, what_web_bin):
-    cmd = WHAT_WEB_CMD.format(what_web_bin=what_web_bin, url=url, output_file=outfile)
+    cmd = WHAT_WEB_CMD.format(
+        what_web_bin=what_web_bin, url=url, output_file=outfile)
     logging.info("执行: %s", cmd)
     os.system(cmd)
 
@@ -21,20 +29,21 @@ def run_whatweb(url, outfile, what_web_bin):
 class WhatWebResultParser(object):
     """whatweb 结果解析"""
 
-    def __init__(self, whatweb_outfile):
+    def __init__(self, whatweb_outfile, name_trans={}):
         with open(whatweb_outfile) as outfile:
             self.whatweb_result = json.load(outfile)
         self.components = {}
-
+        self.name_trans = name_trans
         self.has_attr_MetaGenerator = False
-        self.component_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "component")
-        self.all_components = [component.split(".json")[0] for component in os.listdir(self.component_path) if component.endswith(".json")]
-        self.upper_all_components = [component.upper() for component in self.all_components]
 
     def parse(self):
         for item in self.whatweb_result:
             self._parse_one(item)
         return self.components
+
+    def _trans(self, name):
+        '''转译组件名'''
+        return self.name_trans.get(_safe_lower(name), name)
 
     def _parse_one(self, json_obj):
         """解析某个结果"""
@@ -50,14 +59,14 @@ class WhatWebResultParser(object):
             elif name == "MetaGenerator":
                 self.has_attr_MetaGenerator = True
                 self._parse_meta_generator(plugins[name], pth)
-            elif not self.has_attr_MetaGenerator and (name.upper() in self.upper_all_components):
+            elif not self.has_attr_MetaGenerator and (
+                    _safe_lower(name) in self.name_trans):
                 self._parse_in_plugins_common(plugins[name], name)
-
 
     def _parse_in_plugins_common(self, json_obj, name):
         """组件结果 直接显示在plugins里"""
         version = json_obj.get("version", "")
-        name = self._common_name_calibration(name)
+        name = self._trans(name)
         info = self.components.get(name, {})
         if version:
             info["version"] = version
@@ -73,19 +82,11 @@ class WhatWebResultParser(object):
             elif "(" in item:
                 name, _version = item.split("(")
                 version = _version.split(")")[0]
-            name = self._common_name_calibration(name)
+            name = self._trans(name)
             info = self.components.get(name, {})
             if version:
                 info["version"] = version
             self.components[name] = info
-
-    def _common_name_calibration(self, name):
-        '''转译组件名'''
-        for component in self.all_components:
-            if name.upper() in component.upper():
-                return component
-            else:
-                return name
 
     def _parse_meta_generator(self, json_obj, pth=None):
         """MetaGenerator 结果的解析"""
@@ -96,7 +97,7 @@ class WhatWebResultParser(object):
             elif "." in item:
                 name = item.split(".")[0]
             # name = item.split(" ")[0]
-            name = self._common_name_calibration(name)
+            name = self._trans(name)
             info = self.components.get(name, {})
             if version:
                 info["version"] = version
@@ -109,8 +110,12 @@ def main(what_web_bin="WhatWeb/whatweb"):
     args = parser.parse_args()
     setup_logger()
     _, outfile = tempfile.mkstemp()
+    name_trans = {}
+    for line in open('component_names'):
+        line = line.strip()
+        name_trans[_safe_lower(line)] = line
     run_whatweb(args.url, outfile, what_web_bin)
-    result = WhatWebResultParser(outfile).parse()
+    result = WhatWebResultParser(outfile, name_trans=name_trans).parse()
     print_result(result, args.json_out_file)
 
 
