ba57712bd1086d9deee64ec8869adeee7d66162c
lotuc <lotu.c@outlook.com>
2018-09-20 16:42:56 +0800

    scheduler: 初始化

---
+++ scheduler/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='CScanScheduler',
+      version='1.0',
+      description='CScan Scheduler',
+      author='lotuc',
+      url='',
+      packages=find_packages(),
+      scripts=[])
